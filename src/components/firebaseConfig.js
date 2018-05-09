d7fd61d92c3a8bb5eb87245f0401fffe5a91976e
olzhas2102 <olzhas2102@yahoo.com>
2018-05-09 11:33:26 +0200

    Initial commit - setup

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDmGJw_xNXTAnYhV5yAGQxX_FYnQQc1HeA",
+  authDomain: "employees-list.firebaseapp.com",
+  databaseURL: "https://employees-list.firebaseio.com",
+  projectId: "employees-list",
+  storageBucket: "employees-list.appspot.com",
+  messagingSenderId: "575633532239"
+}
